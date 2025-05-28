42396eb164899e3c4efe4c95485b3d108d12d59c
linh.vtn | apikey:7f55e619c14252ff44a2582ae2f287b4b540735a524a9b6726741310cd877397 <linh.vtn@fsofts.com>
2025-05-28 09:51:43 +0700

    save

--- readme.txt
+++
@@ -1,4 +0,0 @@
-php spark make:migration CreateUsersTable
-php spark cache:clear
-php spark migrate:status
-php spark migrate
--- webpack.mix.js
+++
@@ -1,3 +0,0 @@
-const mix = require('laravel-mix')
-mix.js('public/assets/js/app.js', 'public/assets/build/js')
-   .sass('public/assets/sass/app.scss', 'public/assets/build/css');
