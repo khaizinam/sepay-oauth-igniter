69dbc4b3df76562bf92ae23c919f62cf24784490
linh.vtn | apikey:7f55e619c14252ff44a2582ae2f287b4b540735a524a9b6726741310cd877397 <linh.vtn@fsofts.com>
2025-05-28 11:58:31 +0700

    save

--- app/Controllers/Oauth.php
+++ app/Controllers/Oauth.php
@@ -7,7 +7,6 @@ class Oauth extends BaseController
     public function callback(){
         $code = $this->request->getGet('code');
         $state = $this->request->getGet('state');
-        var_dump($code, $state);
         // Here you would typically exchange the code for an access token
         return redirect()->to(base_url('oauth/callback-success'));
     }
