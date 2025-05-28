32853050ee9e5e5ef0efe01c699138cce1538195
linh.vtn | apikey:7f55e619c14252ff44a2582ae2f287b4b540735a524a9b6726741310cd877397 <linh.vtn@fsofts.com>
2025-05-28 13:39:59 +0700

    save

--- app/Database/Migrations/2025-05-28-043006_AddTableSetting.php
+++ app/Database/Migrations/2025-05-28-043006_AddTableSetting.php
@@ -8,11 +8,57 @@ class AddTableSetting extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 196,
+                'null' => false,
+            ],
+            'description' => [
+                'type' => 'VARCHAR',
+                'constraint' => 196,
+                'null' => false,
+            ],
+            'client_id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 196,
+                'null' => false,
+            ],
+            'redirect_uri' => [
+                'type' => 'VARCHAR',
+                'constraint' => 196,
+                'null' => false,
+            ],
+            'state' => [
+                'type' => 'VARCHAR',
+                'constraint' => 196,
+                'null' => false,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('app_oauths');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('app_oauths');
     }
 }
