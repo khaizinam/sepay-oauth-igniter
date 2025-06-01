2db1a95b2960c4ba71229aaf8a2ee0dab1d89fef
linh.vtn | apikey:7f55e619c14252ff44a2582ae2f287b4b540735a524a9b6726741310cd877397 <linh.vtn@fsofts.com>
2025-06-01 23:48:38 +0700

    save

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -4,9 +4,12 @@
             <thead>
                 <tr>
                     <th scope="col">ID</th>
+                    <th scope="col">ID DG</th>
                     <th scope="col">Tài khoản</th>
                     <th scope="col">Tài khoản ảo</th>
                     <th scope="col">Tiền</th>
+                    <th scope="col">Loại giao dịch</th>
+                    <th scope="col">Nội dung chuyển khoản</th>
                 </tr>
             </thead>
             <tbody id="transactionTableBody">
@@ -14,6 +17,9 @@
                     <tr>
                         <th scope="row">
                             <?= app_get_data($item, 'id') ?>
+                        </th>
+                        <th>
+                            <?= app_get_data($item, 'transaction_id') ?>
                         </th>
                         <td>
                             <?= app_get_data($item, 'account_number') ?>
@@ -23,6 +29,12 @@
                         </td>
                         <td>
                             <?= app_get_data($item, 'amount_out') > 0 ? '-' . app_get_data($item, 'amount_out') : '+' .app_get_data($item, 'amount_in') ?>
+                        </td>
+                        <td>
+                            <?= app_get_data($item, 'amount_out') > 0 ? 'Tiền ra' : 'Tiền vào' ?>
+                        </td>
+                        <td>
+                            <?= app_get_data($item, 'transaction_content') ?>
                         </td>
                     </tr>
                 <?php endforeach; ?>
